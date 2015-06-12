b7130800ea5a82315c3b000ffb55a6f647e0d43e
Christophe Cassagnabère <cassagnachristophe@hotmail.com>
2015-06-12 12:06:00 +0200

    Cleaning targets

--- dbunit/target/test-classes/org/javagems/dbunit/mydb.customer.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS mydb.customer;
-CREATE TABLE mydb.customer (
-    id         INT NOT NULL AUTO_INCREMENT
-   ,first_name VARCHAR(30) NOT NULL
-   ,last_name  VARCHAR(30) NOT NULL
-   ,initial    VARCHAR(1) NULL
-   ,PRIMARY KEY(id)
-);
